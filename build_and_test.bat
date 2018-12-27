3ea65826a5723bf0b21e2591da9866b080de6542
Dmitry Ledentsov <dmlled@yahoo.com>
2018-12-27 18:02:17 +0100

    appveyor: fail on test error

--- build_and_test.bat
+++ build_and_test.bat
@@ -1,3 +1,5 @@
 dotnet test wigc_test\wigc_test.csproj
+IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 dotnet run --project willitgocd\willitgocd.csproj -- help
 dotnet run --project willitgocd\willitgocd.csproj -- xml -f wigc_test\data\with_environments.xml
